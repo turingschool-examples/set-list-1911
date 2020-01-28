7d3a9d6e934aac20a42a07904aaceeebb28961b0
Michael Dao <mike@turing.io>
2020-01-28 10:56:30 -0700

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Song.create(title: "I Really Like You", length: 209, play_count: 760847)
+Song.create(title: "Call Me Maybe", length: 199, play_count: 65862)
+Song.create(title: "Run Away With Me", length: 253, play_count: 521771)
+Song.create(title: "Party For One", length: 269, play_count: 623547)
