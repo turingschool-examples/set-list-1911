62908575bca8416810c5c0971474b72e064719d8
Meghan Stang <mstang15@gmail.com>
2020-02-03 12:57:11 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+
+Song.destroy_all
+Artist.destroy_all
+
 carly_rae = Artist.create(name: "Carly Rae")
 
-Song.create(title: "I Really Like You", length: 209, play_count: 760847, artist: carly_rae)
-Song.create(title: "Call Me Maybe", length: 199, play_count: 65862, artist: carly_rae)
-Song.create(title: "Run Away With Me", length: 253, play_count: 521771, artist: carly_rae)
-Song.create(title: "Party For One", length: 269, play_count: 623547, artist: carly_rae)
+Song.create(title: "I Really Like You", length: 300, play_count: 1000, artist: carly_rae)
+Song.create(title: "Call Me Maybe", length: 200, play_count: 3000, artist: carly_rae)
+Song.create(title: "Run Away With Me", length: 100, play_count: 2000, artist: carly_rae)
+Song.create(title: "Party For One", length: 50, play_count: 5000, artist: carly_rae)
